6718ac4ca57c8e514becae6791127c052b56ff43
Miguel Ángel Bernal Valdéz <dm.mbernal@gmail.com>
2022-11-22 21:39:39 -0300

    Primer commit

---
+++ 01_hola_mundo.py
@@ -0,0 +1,28 @@
+# funciones internas de python
+# nombre de la funcion
+#    (parametros)
+print("Hola Mundo desde Python")
+print('Hola Mundo desde Python con comilla simple')
+print("""Hola Mundo desde Python 
+         con comilla simple""")
+print('''Hola Mundo desde Python 
+         con comilla simple''')
+print(10,20,"Hola","Adios")
+# variable
+# caja = 'zapatos'
+# caja = 'lapices'
+nombre = 'Andrés' # Tipo de dato string
+apellido = 'González' 
+edad = 26 # Tipo de dato numérico (Integer o entero)
+numero1 = '10'
+numero2 = '20'
+numero3 = 50.27 # Tipo de dato (Float o decimales)
+numero4 = 70.35
+estudia = True # Tipo de dato booleano (True, False)
+print(nombre + " " + apellido + " que tiene " + str(edad) + " años.")
+#                  operadores de concatenacion
+print(int(numero1) + int(numero2))
+#             operadores matematicos 
+print(numero3 + numero4)
+print("Estudia? = " + str(estudia))
+print('Continuando con el programa...')
