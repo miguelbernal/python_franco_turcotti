742c1bf7d76995b279dc29e2ee9b4c5265f4c841
Miguel Ángel Bernal Valdéz <dm.mbernal@gmail.com>
2022-11-23 00:47:22 -0300

    Estructuras

---
+++ 02_variables.py
@@ -0,0 +1,8 @@
+nombre = 'Juan González' # string
+edad = 18
+estudia = True
+calificacion = 4.5
+print("Me llamo " + nombre + " y tengo " + str(edad) + " y estudio = " + str(estudia) + " y quité " + str(calificacion) + " en Matemáticas.")
+print(nombre.upper())
+print(nombre.lower())
+print(nombre.title())
